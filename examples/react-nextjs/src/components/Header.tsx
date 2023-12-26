f46ef8c50ea825f1debb12dcbf809087ee960eba
Paulo Vitor <pvitorv@gmail.com>
2023-12-26 08:24:55 -0300

    006

--- examples/react-nextjs/src/components/Header.tsx
+++ examples/react-nextjs/src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
         <Link
           href="/"
           className="d-flex align-items-center text-dark text-decoration-none"
-        >
+          legacyBehavior>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32"
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,20 +5,26 @@ import Link from 'next/link';
 import Image from 'next/image';
 import thumbImg from '../../../public/images/thumb.png';
 export default function Blog() {
-    return (
-    <>
-    <Head>
-      <title>Blog | Ponto De Impacto</title>
+    return <>
+    <Head >
+      <title className={styles.principio}>Blog | Ponto De Impacto</title>
     </Head>
-    <main className={styles.containe}>
+    <h1 className={styles.blog}>Blog | Ponto De Ipacto</h1>
+    <main className={styles.container}>
+    
+      
       <div className={styles.posts}>
+
         <Link href="/">
           <a>
-            <Image src={thumbImg} alt="Post titulo 1"/>
+            <Image src={thumbImg} alt="Post titulo 1" width={720} height={410} quality={100}/>
+            <strong>Como usar nossa ferramenta</strong>
+            <time>25 DEZEMBRO 2023</time>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto at ipsam sequi, voluptatum fuga ex.</p>
           </a>
         </Link>
+
       </div>
     </main>
-    </>
-    )
+    </>;
   }
